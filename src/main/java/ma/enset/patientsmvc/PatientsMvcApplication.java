32e4815805e52e53d41f69a1a1344c589cf657d7
Namasse Zineb <z.namasse@gmail.com>
2022-04-09 12:39:18 +0000

    Partie2

--- src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
+++ src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
@@ -21,13 +21,13 @@ public class PatientsMvcApplication {
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args -> {
             patientRepository.save(
-                    new Patient(null,"Hassan",new Date(),false,12));
+                    new Patient(null,"Hassan",new Date(),false,122));
             patientRepository.save(
                     new Patient(null,"Mohammed",new Date(),true,321));
             patientRepository.save(
-                    new Patient(null,"Yasmine",new Date(),true,65));
+                    new Patient(null,"Yasmine",new Date(),true,165));
             patientRepository.save(
-                    new Patient(null,"Hanae",new Date(),false,32));
+                    new Patient(null,"Hanae",new Date(),false,132));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
